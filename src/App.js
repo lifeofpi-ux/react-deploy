56615bc1e03e5c92446a4610b720844ce7268d49
lifeofpi-ux <indend007@gmail.com>
2024-12-11 14:25:19 +0900

    react-start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-pink-300 via-pink-400 to-pink-500">
+      <div className="bg-white/30 backdrop-blur-md rounded-xl p-8 shadow-lg">
+        <h1 className="text-4xl font-pretendard font-bold text-white text-center">
+          Hello World
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
